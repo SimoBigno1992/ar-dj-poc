ff1515d91417411d8a3c1116b92e40b982984768
Simone Bignotti <simone.bignotti@alten.it>
2024-04-10 11:11:43 +0200

    trying using aframeù

--- src/components/viewer.tsx
+++ src/components/viewer.tsx
@@ -1,5 +1,6 @@
 //@ts-nocheck
 "use client";
+import {Entity, Scene } from 'aframe-react';
 import QRCode from 'react-qr-code';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -33,19 +34,22 @@ function Viewer() {
 
 	return (
 		<>
-			<model-viewer
-				className={(isMobileDevice ? 'model-viewer-mobile' : '')}
-				src={'/nike.glb#callToAction=Browse%20API&checkoutTitle=Kids%20Slide&checkoutSubtitle=Playground%20in%20your%20backyard&price=$145'}
-				ios-src={'/nike.usdz#callToAction=Browse%20API&checkoutTitle=Kids%20Slide&checkoutSubtitle=Playground%20in%20your%20backyard&price=$145'}
-				alt='model name'
-				ar
-				loading='lazy'
-				camera-controls
-				autoplay>
-					<button onClick={openAR}>
-							Anteprima in AR
-					</button>
-			</model-viewer>
+			<Scene>
+				<Entity plane visible="false" geometry={{ primitive: 'plane' }} /> {/* Plane for detection */}
+				<model-viewer
+					className={(isMobileDevice ? 'model-viewer-mobile' : '')}
+					src={'/nike.glb'}
+					ios-src={'/nike.usdz#callToAction=Browse%20API&checkoutTitle=Kids%20Slide&checkoutSubtitle=Playground%20in%20your%20backyard&price=$145'}
+					alt='model name'
+					ar
+					loading='lazy'
+					camera-controls
+					autoplay>
+						<button onClick={openAR}>
+								Anteprima in AR
+						</button>
+				</model-viewer>
+			</Scene>
 			{message && (
 				<div className='desktop-ar-container'>
 					<QRCode value={`${window.location.href}api/redirect`} size={80} />
